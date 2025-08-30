91922d63917f76e255aac1c945247169537e60ca
vedaRadev <ryancarmstrong97@gmail.com>
2025-08-30 16:57:18 -0400

    create and display a simple window

--- build.bat
+++ build.bat
@@ -6,4 +6,4 @@ IF NOT EXIST %BUILD_DIR% mkdir %BUILD_DIR%
 pushd %BUILD_DIR%
 
 REM /Zi - include debug info
-cl ..\platform_win32.cpp /Zi /link user32.lib
+cl ..\platform_win32.cpp /std:c++20 /Zi /link user32.lib
--- platform_win32.cpp
+++ platform_win32.cpp
@@ -1,6 +1,60 @@
 #include <windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
-    OutputDebugStringA("Hello World!");
+LRESULT CALLBACK window_proc(HWND hwnd, UINT msg, WPARAM w_param, LPARAM l_param) {
+    return DefWindowProc(hwnd, msg, w_param, l_param);
+}
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int cmd_show) {
+    WNDCLASS game_window_class = {
+        .lpfnWndProc = window_proc,
+        .hInstance = instance,
+        // TODO(ryan): Better window class name
+        .lpszClassName = "GameWindowClass",
+    };
+    if (!RegisterClassA(&game_window_class)) {
+        MessageBoxA(NULL, "Failed to register window class", "Error", MB_OK);
+        return 1;
+    }
+
+    // TODO(ryan): check if there are different options we want to use for anything
+    HWND game_window = CreateWindowExA(
+        0,
+        game_window_class.lpszClassName,
+        // TODO(ryan): different name
+        "Game From Scratch",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        instance,
+        NULL
+    );
+    if (game_window == NULL) {
+        MessageBoxA(NULL, "Failed to create game window", "Error", MB_OK);
+        return 1;
+    }
+    ShowWindow(game_window, cmd_show);
+
+    bool game_is_running = true;
+    // TODO(ryan): we need to be able to set a target framerate and sleep (or
+    // spin, if we can't make our sleep granular) if we process everything too
+    // fast.
+    while (game_is_running) {
+        MSG window_message;
+        while (PeekMessageA(&window_message, 0, 0, 0, PM_REMOVE)) {
+            switch (window_message.message) {
+                case WM_QUIT:
+                    game_is_running = false;
+                    break;
+
+                default:
+                    TranslateMessage(&window_message);
+                    DispatchMessageA(&window_message);
+                    break;
+            }
+        }
+    }
+    
+
     return 0;
 }
