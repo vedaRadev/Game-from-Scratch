e1f327dc9aa3a3979d81e783c6b2ccb65c2e4467
vedaRadev <ryancarmstrong97@gmail.com>
2025-08-31 11:34:37 -0400

    handle window closing, app shutdown

--- platform_win32.cpp
+++ platform_win32.cpp
@@ -1,7 +1,22 @@
 #include <windows.h>
 
-LRESULT CALLBACK window_proc(HWND hwnd, UINT msg, WPARAM w_param, LPARAM l_param) {
-    return DefWindowProc(hwnd, msg, w_param, l_param);
+static bool game_is_running = false;
+
+LRESULT CALLBACK window_proc(HWND window, UINT message, WPARAM w_param, LPARAM l_param) {
+    LRESULT result = 0;
+
+    switch (message) {
+        case WM_CLOSE: // TODO(ryan): handle WM_CLOSE as an error and recreate the window
+        case WM_DESTROY:
+            game_is_running = false;
+            break;
+
+        default:
+            result = DefWindowProcA(window, message, w_param, l_param);
+            break;
+    }
+
+    return result;
 }
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int cmd_show) {
@@ -35,10 +50,10 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, 
     }
     ShowWindow(game_window, cmd_show);
 
-    bool game_is_running = true;
     // TODO(ryan): we need to be able to set a target framerate and sleep (or
     // spin, if we can't make our sleep granular) if we process everything too
     // fast.
+    game_is_running = true;
     while (game_is_running) {
         MSG window_message;
         while (PeekMessageA(&window_message, 0, 0, 0, PM_REMOVE)) {
@@ -54,7 +69,8 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, 
             }
         }
     }
-    
+
+    MessageBoxA(NULL, "Game is shutting down", "Info", MB_OK);
 
     return 0;
 }
